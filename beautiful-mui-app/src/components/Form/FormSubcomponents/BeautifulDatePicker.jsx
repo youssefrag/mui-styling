0e12ed53d62d67f9a6549832866b626cd4a4a381
Youssef Ragab <ragab.youssef95@gmail.com>
2022-09-27 14:05:43 -0400

    finish styling date picker

--- beautiful-mui-app/src/components/Form/FormSubcomponents/BeautifulDatePicker.jsx
+++ beautiful-mui-app/src/components/Form/FormSubcomponents/BeautifulDatePicker.jsx
@@ -1,17 +1,43 @@
 import { TextField } from "@mui/material";
+import { CalendarToday } from "@mui/icons-material";
 import { minWidth } from "../ContactForm";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+
+const popperSx = {
+  "& .MuiPaper-root": {
+    color: "yellow",
+  },
+  "& [role=grid]": {
+    backgroundColor: "rgba(0,0,0,0.1)",
+    "& button": {
+      backgroundColor: "rgba(0,0,0,0.2)",
+    },
+  },
+};
 
 export const BeautifulDatePicker = (props) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DesktopDatePicker
         {...props}
+        label="Date"
+        views={["day"]}
         inputFormat="MM/DD/YYYY"
         renderInput={(params) => {
           return <TextField {...params} sx={{ minWidth: minWidth }} />;
         }}
+        InputProps={{
+          sx: {
+            "& .MuiSvgIcon-root": {
+              color: "primary.main",
+            },
+          },
+        }}
+        components={{
+          OpenPickerIcon: CalendarToday,
+        }}
+        PopperProps={{ sx: popperSx }}
       />
     </LocalizationProvider>
   );
